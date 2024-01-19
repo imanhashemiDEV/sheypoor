5a5eec78dcd5dbec534efa63772f212adcaccad8
iman hashemi <hashemi.iman@gmail.com>
2024-01-19 15:11:05 +0330

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '0.1rem'
+    },
+    fontFamily: {
+      'vazir': "vazir",
+    },
     extend: {},
   },
   plugins: [],
