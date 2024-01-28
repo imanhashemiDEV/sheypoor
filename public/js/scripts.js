ce190a759d9ed9dd2499ffa44436881206120e9c
iman hashemi <hashemi.iman@gmail.com>
2024-01-28 23:01:38 +0330

    footer

---
+++ public/js/scripts.js
@@ -0,0 +1,9 @@
+
+let likes = document.querySelectorAll('.like')
+
+likes.forEach(like => {
+    like.addEventListener('click',function(e){
+        e.preventDefault();
+        console.log('here');
+    })
+});
